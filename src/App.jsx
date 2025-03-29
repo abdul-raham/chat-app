dd664d29f09309bfa4d8ee20324ab64abc1b766b
Abdulrahman <abdulrahmanbakare001@gmail.com>
2025-03-29 18:43:25 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";  
-import Register from "./components/Register";  
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
